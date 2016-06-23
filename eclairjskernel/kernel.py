95e8a02d971a87a91a3b364e77486cb122df8733
Brian Burns <bburns@us.ibm.com>
2016-06-23 15:47:19 -0400

    allow java to call python

--- eclairjskernel/kernel.py
+++ eclairjskernel/kernel.py
@@ -2,9 +2,11 @@ import subprocess
 import os
 import sys
 from metakernel import MetaKernel
-#from metakernel import pexpect
 import pexpect
-from py4j.java_gateway import JavaGateway
+from py4j.java_gateway import JavaGateway, CallbackServerParameters, java_import
+#from py4j.clientserver import ClientServer, JavaParameters, PythonParameters
+
+from py4j.protocol import Py4JError
 
 class TextOutput(object):
     """Wrapper for text output whose repr is the text itself.
@@ -15,6 +17,19 @@ class TextOutput(object):
 
     def __repr__(self):
         return self.output
+
+class ForeachRDDListener(object):
+
+    def __init__(self, comm):
+        self.comm = comm
+
+    def on_rdd(self, msg):
+        self.comm.send(msg)
+        return None
+
+    class Java:
+        implements = ['org.eclairjs.nashorn.IForeachRDD']
+
 
 class EclairJSKernel(MetaKernel):
     implementation = 'eclairjs_kernel'
@@ -29,28 +44,49 @@ class EclairJSKernel(MetaKernel):
     _SPARK_COMMAND = '{}/bin/spark-submit'.format(os.environ['SPARK_HOME'])
     _ECLAIRJS_LOCATION = os.environ['ECLAIRJS_NASHORN_HOME']
 
+    class Java:
+        implements = ['org.eclairjs.nashorn.IForeachRDD']
+
+
     def __init__(self, **kwargs):
         super(EclairJSKernel, self).__init__(**kwargs)
-        self.comm_manager.register_target('foreachrdd',
-                handle_comm_opened)
+        self.comm_manager.register_target('foreachrdd', self.comm_opened)
         self._start_eclairjs_server()
 
-    def _start_eclairjs_server(self):
-        args = "{} --class org.eclairjs.nashorn.EclairJSGatewayServer --name EclairJSShell {}".format(self._SPARK_COMMAND, self._ECLAIRJS_LOCATION)
+    def on_rdd(self, id, msg):
+        self.log.error("got it " + id)
+        self.comm_manager.get_comm(id).send(msg)
 
+    def _start_eclairjs_server(self):
+        #args = "{} --class org.eclairjs.nashorn.EclairJSGatewayServer --name EclairJSShell {}".format(self._SPARK_COMMAND, self._ECLAIRJS_LOCATION)
+        args = "{} --class org.eclairjs.nashorn.EclairJSGatewayServer --name EclairJSShell --conf spark.driver.userClassPathFirst=true {}".format(self._SPARK_COMMAND, self._ECLAIRJS_LOCATION)
+
+        self.log.error(args)
         self.gateway_proc = pexpect.spawnu(args)
         self.gateway_proc.expect("Gateway Server Started")
+        self.gateway = JavaGateway(
+                start_callback_server=True,
+                callback_server_parameters=CallbackServerParameters(),
+                python_server_entry_point=self)
         """
-        self.gateway_proc = subprocess.Popen([
-                self._SPARK_COMMAND,
-                "--class",
-                "org.eclairjs.nashorn.EclairJSGatewayServer",
-                "--name", 
-                "EclairJSShell",
-                self._ECLAIRJS_LOCATION
-                ], stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=0)
+        try:
+            clazz = self.gateway.jvm.java.lang.Thread.currentThread().getContextClassLoader().loadClass("org.eclairjs.nashorn.IForeachRDD")
+            if clazz:
+                self.log.error("clazz = " + clazz.getName())
+            else:
+                self.log.error("class not found")
+        except Py4JError as e:
+            self.log.error(e.cause)
         """
-        self._gateway = JavaGateway()
+        #self.gateway = ClientServer(
+        #        java_parameters=JavaParameters(),
+        #        python_parameters=PythonParameters())
+
+    def comm_opened(self, comm, msg):
+        self.log.error("comm opened " + comm.comm_id)
+        #listener = ForeachRDDListener(comm)
+        #self.gateway.entry_point.registerForeachRDD(comm.comm_id,listener)
+        pass
 
     def do_execute_direct(self, code, silent=False):
         """
@@ -68,7 +104,8 @@ class EclairJSKernel(MetaKernel):
 
         retval = None
         try:
-            retval = self._gateway.entry_point.eval(code.rstrip())
+            retval = self.gateway.entry_point.eval(code.rstrip())
+            #self.log.error("here")
             self.gateway_proc.expect("eclairjs_done_execute")
             txt = self.gateway_proc.before
             if txt and txt.strip():
