04ce0ac1fcd57003cfc2c151d8d551f433647967
Brian Burns <bburns@us.ibm.com>
2016-06-24 15:40:29 -0400

    use subprocess and read stdout / stderr non blocking

--- eclairjskernel/kernel.py
+++ eclairjskernel/kernel.py
@@ -1,11 +1,13 @@
-import subprocess
 import os
+import signal
 import sys
-from metakernel import MetaKernel
-import pexpect
-from py4j.java_gateway import JavaGateway, CallbackServerParameters, java_import
-#from py4j.clientserver import ClientServer, JavaParameters, PythonParameters
+import time
 
+from os import O_NONBLOCK, read
+from fcntl import fcntl, F_GETFL, F_SETFL
+from  subprocess import Popen, PIPE
+from metakernel import MetaKernel
+from py4j.java_gateway import JavaGateway, CallbackServerParameters, java_import
 from py4j.protocol import Py4JError
 
 class TextOutput(object):
@@ -20,15 +22,17 @@ class TextOutput(object):
 
 class ForeachRDDListener(object):
 
-    def __init__(self, comm):
-        self.comm = comm
+    def __init__(self, kernel):
+        self.kernel = kernel
 
-    def on_rdd(self, msg):
-        self.comm.send(msg)
+    def call(self, comm_id, msg):
+        self.kernel.log.error("got message " + msg)
+        self.kernel.comm_manager.get_comm(comm_id).send(msg)
+        self.kernel.log.error("message sent")
         return None
 
     class Java:
-        implements = ['org.eclairjs.nashorn.IForeachRDD']
+        implements = ['org.apache.spark.api.java.function.Function2']
 
 
 class EclairJSKernel(MetaKernel):
@@ -44,49 +48,50 @@ class EclairJSKernel(MetaKernel):
     _SPARK_COMMAND = '{}/bin/spark-submit'.format(os.environ['SPARK_HOME'])
     _ECLAIRJS_LOCATION = os.environ['ECLAIRJS_NASHORN_HOME']
 
-    class Java:
-        implements = ['org.eclairjs.nashorn.IForeachRDD']
-
 
     def __init__(self, **kwargs):
         super(EclairJSKernel, self).__init__(**kwargs)
         self.comm_manager.register_target('foreachrdd', self.comm_opened)
         self._start_eclairjs_server()
 
-    def on_rdd(self, id, msg):
-        self.log.error("got it " + id)
-        self.comm_manager.get_comm(id).send(msg)
+    def sig_handler(signum, frame):
+        self.gateway_proc.terminate()
+
+    def do_shutdown(self, restart):
+        super(EclairJSKernel, self).do_shutdown(restart)
+        self.gateway_proc.terminate()
 
     def _start_eclairjs_server(self):
-        #args = "{} --class org.eclairjs.nashorn.EclairJSGatewayServer --name EclairJSShell {}".format(self._SPARK_COMMAND, self._ECLAIRJS_LOCATION)
-        args = "{} --class org.eclairjs.nashorn.EclairJSGatewayServer --name EclairJSShell --conf spark.driver.userClassPathFirst=true {}".format(self._SPARK_COMMAND, self._ECLAIRJS_LOCATION)
+        args = [
+            self._SPARK_COMMAND,
+            "--class",
+            "org.eclairjs.nashorn.EclairJSGatewayServer",
+            "--name",
+            "EclairJSShell",
+            self._ECLAIRJS_LOCATION
+        ]
 
-        self.log.error(args)
-        self.gateway_proc = pexpect.spawnu(args)
-        self.gateway_proc.expect("Gateway Server Started")
+        self.gateway_proc = Popen(args, stdout=PIPE, stderr=PIPE)
+        time.sleep(5)
         self.gateway = JavaGateway(
                 start_callback_server=True,
-                callback_server_parameters=CallbackServerParameters(),
-                python_server_entry_point=self)
-        """
-        try:
-            clazz = self.gateway.jvm.java.lang.Thread.currentThread().getContextClassLoader().loadClass("org.eclairjs.nashorn.IForeachRDD")
-            if clazz:
-                self.log.error("clazz = " + clazz.getName())
-            else:
-                self.log.error("class not found")
-        except Py4JError as e:
-            self.log.error(e.cause)
-        """
-        #self.gateway = ClientServer(
-        #        java_parameters=JavaParameters(),
-        #        python_parameters=PythonParameters())
+                callback_server_parameters=CallbackServerParameters())
+
+        flags = fcntl(self.gateway_proc.stdout, F_GETFL) # get current p.stdout flags
+        fcntl(self.gateway_proc.stdout, F_SETFL, flags | O_NONBLOCK)
+
+        flags = fcntl(self.gateway_proc.stderr, F_GETFL) # get current p.stdout flags
+        fcntl(self.gateway_proc.stderr, F_SETFL, flags | O_NONBLOCK)
+
+        signal.signal(signal.SIGTERM, self.sig_handler)
+        signal.signal(signal.SIGINT, self.sig_handler)
+        signal.signal(signal.SIGHUP, self.sig_handler)
+
 
     def comm_opened(self, comm, msg):
         self.log.error("comm opened " + comm.comm_id)
-        #listener = ForeachRDDListener(comm)
-        #self.gateway.entry_point.registerForeachRDD(comm.comm_id,listener)
-        pass
+        cb = ForeachRDDListener(self)
+        self.gateway.entry_point.registerCallback(cb)
 
     def do_execute_direct(self, code, silent=False):
         """
@@ -104,13 +109,20 @@ class EclairJSKernel(MetaKernel):
 
         retval = None
         try:
+            #self.log.error(code)
             retval = self.gateway.entry_point.eval(code.rstrip())
-            #self.log.error("here")
-            self.gateway_proc.expect("eclairjs_done_execute")
-            txt = self.gateway_proc.before
-            if txt and txt.strip():
-                self.Print(txt.strip())
-            
+
+            while True:
+                try:
+                    self.Print(read(self.gateway_proc.stdout.fileno(), 1024))
+                except OSError:
+                    break
+
+            while True:
+                try:
+                    self.Error(read(self.gateway_proc.stderr.fileno(), 1024))
+                except OSError:
+                    break
         except Py4JError as e:
             if not silent:
                 self.Error(e.cause)
